23b47f6d2576c4e5ea294e05967b977d2c79ba6a
user <hahynda@gmail.com>
2010-07-20 18:45:48 +0300

    Интерфейсы "внешнего объекта" непосредственно после создания объекта "внутренняя машина".

--- externals/object/interface.h
+++ externals/object/interface.h
@@ -1,18 +1,39 @@
 #pragma once
 
-#include "internals/globals.h"
+#include "internals/basics.h"
 #include "internals/string_map/interface.h"
+#include "internals/string_set/interface.h"
+#include "internals/list/interface.h"
+#include "internals/string/interface.h"
 
-typedef struct {
-	int			used;
-	int			ptr_entity;
-	SvvInternalObject	entity;
-	SvvInternalStringMap	internals;
-	SvvInternalStringMap	methods;
-	SvvInternalStringMap	messages;
+typedef struct SvvExternalObject {
+	struct SvvExternalObject*	parent;
+	int				links;
+	int				entity_type;
+	SvvInternalObject		entity;
+	SvvInternalStringSet		properties;
+	SvvInternalStringMap		fields;
+	SvvInternalSet			methods;
+	SvvInternalStringMap		message_handlers;
+	SvvInternalList			code;
+	SvvInternalString		token;
+	int				token_type;
 } *SvvExternalObject;
 
-SvvInternalAction(SvvExternalObject, Create, SvvExternalObject);
+SvvInternalCreator(SvvExternalObject);
 SvvInternalAction(SvvExternalObject, Destroy, void);
 SvvInternalAction(SvvExternalObject, Link, void);
-SvvInternalAction(SvvExternalObject, Unlink, void);
+SvvInternalAction(SvvExternalObject, UnLink, void);
+SvvInternalAction(SvvExternalObject, Clone, SvvExternalObject);
+SvvInternalAction(SvvExternalObject, Derive, SvvExternalObject);
+SvvInternalAction(SvvExternalObject, SetParent, void, SvvExternalObject Parent);
+
+SvvInternalAction(SvvExternalObject, PropertySet, void, SvvInternalString Property);
+SvvInternalAction(SvvExternalObject, PropertyUnSet, void, SvvInternalString Property);
+SvvInternalAction(SvvExternalObject, PropertyTest, int, SvvInternalString Property);
+
+SvvInternalAction(SvvExternalObject, CodeSet, void, SvvInternalList Code);
+
+SvvInternalAction(SvvExternalObject, Run, void);
+
+
