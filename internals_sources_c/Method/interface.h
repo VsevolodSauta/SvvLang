489b5c78334ac296bd8c19df6fb64c2d9b109918
Vsevolod Sauta <hahynda@gmail.com>
2011-07-30 09:07:31 +0300

    Исправлены интерфейсы Метода.

--- internals_sources_c/Method/interface.h
+++ internals_sources_c/Method/interface.h
@@ -3,7 +3,7 @@
 #include "internals/Object/interface.h"
 
 Object Method_Create();
-Object Method_Invoke(Object _self, ...);
+Object Method_Invoke(Object _self, Object _object, ...);
 Object Method_Compare(Object, Object);
 Object Method_Destroy(Object);
 Object Method_Clone(Object);
